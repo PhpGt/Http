224a46746ae19fb251fd7fc25c8491a4304a150d
Greg Bowler <greg.bowler@g105b.com>
2017-11-04 14:46:44 +0000

    Drop get/post as this should be handled in Input classes

--- src/RequestFactory.php
+++ src/RequestFactory.php
@@ -2,7 +2,7 @@
 namespace Gt\Http;
 
 class RequestFactory {
-	public static function create(array $server, array $get, array $post):Request {
+	public static function create(array $server):Request {
 		$serverInfo = new ServerInfo($server);
 		$uri = new Uri($serverInfo->getRequestUri());
 		$headers = new RequestHeaders($serverInfo->getHttpHeadersArray());
