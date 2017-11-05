12d56c55403d7829d7d38a14e3830523f8f06491
Greg Bowler <greg.bowler@g105b.com>
2017-11-05 22:37:28 +0000

    To and from internal array

--- src/Headers.php
+++ src/Headers.php
@@ -2,7 +2,17 @@
 namespace Gt\Http;
 
 abstract class Headers {
-	public function __construct(array $headerArray) {
+	protected $headerArray;
 
+	public function __construct(array $headerArray) {
+		$this->fromArray($headerArray);
+	}
+
+	public function asArray():array {
+		return $this->headerArray;
+	}
+
+	public function fromArray(array $headerArray):void {
+		$this->headerArray = $headerArray;
 	}
 }
