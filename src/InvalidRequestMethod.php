a5e98e0161ee090d8f3b683b3140f5420f41ef84
Greg Bowler <greg.bowler@g105b.com>
2017-11-05 22:36:07 +0000

    RequestMethod filtering class

---
+++ src/InvalidRequestMethod.php
@@ -0,0 +1,4 @@
+<?php
+namespace Gt\Http;
+
+class InvalidRequestMethod extends Exception {}
---
+++ src/RequestMethod.php
@@ -0,0 +1,24 @@
+<?php
+namespace Gt\Http;
+
+class RequestMethod {
+	const ALLOWED_METHODS = [
+		"GET",
+		"HEAD",
+		"POST",
+		"PUT",
+		"OPTIONS",
+		"DELETE",
+		"TRACE",
+	];
+
+	public static function filterMethodName(string $name):string {
+		$name = strtoupper($name);
+
+		if(!in_array($name, self::ALLOWED_METHODS)) {
+			throw new InvalidRequestMethod($name);
+		}
+
+		return $name;
+	}
+}
