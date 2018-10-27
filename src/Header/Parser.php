24f6fca0a124e082c62189b4f1d5d4d79024b0a5
Greg Bowler <greg.bowler@g105b.com>
2018-10-27 17:01:44 +0100

    Parse headers from raw string

---
+++ src/Header/Parser.php
@@ -0,0 +1,48 @@
+<?php
+namespace Gt\Http\Header;
+
+class Parser {
+	protected $rawHeaders;
+
+	public function __construct(string $rawHeaders) {
+		$this->rawHeaders = $rawHeaders;
+	}
+
+	public function getProtocolVersion():string {
+		return $this->pregMatchProtocol("version");
+	}
+
+	public function getStatusCode():int {
+		return $this->pregMatchProtocol("code");
+	}
+
+	public function getKeyValues():array {
+		$keyValues = [];
+
+		foreach(explode("\n", $this->rawHeaders) as $i => $h) {
+			if($i === 0) {
+				continue;
+			}
+
+			$h = trim($h);
+			list(
+				$key,
+				$value
+			) = explode(":", $h, 2);
+
+			$keyValues[$key] = $value;
+		}
+
+		return $keyValues;
+	}
+
+	protected function pregMatchProtocol(string $matchName) {
+		$headerLine = strtok($this->rawHeaders, "\n");
+		preg_match(
+			"/HTTP\/(?P<version>.+) (?P<code>\d+) .+/",
+			$headerLine,
+			$matches
+		);
+		return $matches[$matchName];
+	}
+}
