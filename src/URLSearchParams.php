c17e5cc2b14f326207fa77b54dd113238b9a5ae5
Greg Bowler <greg.bowler@g105b.com>
2023-05-04 12:09:46 +0100

    feature: implement URLSearchParams

--- src/URLSearchParams.php
+++ src/URLSearchParams.php
@@ -1,6 +1,8 @@
 <?php
 namespace Gt\Http;
 
+use Countable;
+use Generator;
 use Gt\PropFunc\MagicProp;
 use Gt\TypeSafeGetter\NullableTypeSafeGetter;
 use Stringable;
@@ -9,12 +11,16 @@ use Stringable;
  * @property-read int $size The read-only URLSearchParams.size property
  * indicates the total number of search parameter entries.
  */
-class URLSearchParams implements Stringable {
+class URLSearchParams implements Stringable, Countable {
 	use MagicProp;
 	use NullableTypeSafeGetter;
 
+	/** @var array<string, string|array<string>> */
+	private array $kvp;
+
 	/**
-	 * @param string|array|object|FormData $options Options is one of:
+	 * @param string|array<string, string>|FormData $options
+	 * Options is one of:
 	 * 1) A string, which will be parsed from
 	 * application/x-www-form-urlencoded format. A leading '?' character
 	 * is ignored.
@@ -27,19 +33,41 @@ class URLSearchParams implements Stringable {
 	 * not supported.
 	 */
 	public function __construct(
-		private string|array|FormData $options
-	) {}
+		string|array|FormData $options = ""
+	) {
+		if(is_string($options)) {
+			$options = trim($options, "?");
+			parse_str($options, $query);
+			/** @var array<string, string> $query */
+			$this->kvp = $query;
+		}
+		elseif($options instanceof FormData) {
+
+		}
+		else {
+			$this->kvp = $options;
+		}
+	}
 
 	/**
 	 * @return string A string, without the question mark. Returns an
 	 * empty string if no search parameters have been set.
 	 */
 	public function __toString():string {
-
+		$string = http_build_query($this->kvp);
+		return preg_replace(
+			"/%5B(\d)%5D/",
+			"[]",
+			$string
+		);
 	}
 
-	public function __get_size():int {
+	public function count():int {
+		return count($this->kvp);
+	}
 
+	public function __prop_get_size():int {
+		return count($this);
 	}
 
 	/**
@@ -51,9 +79,24 @@ class URLSearchParams implements Stringable {
 	 *
 	 * @param string $name The name of the parameter to append.
 	 * @param string $value The value of the parameter to append.
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/append
 	 */
 	public function append(string $name, string $value):void {
-
+		if(is_array($this->kvp[$name])) {
+			array_push($this->kvp[$name], $value);
+		}
+		else {
+			if(isset($this->kvp[$name])) {
+				$this->kvp[$name] = [
+					$this->kvp[$name],
+					$value,
+				];
+			}
+			else {
+				$this->set($name, $value);
+			}
+		}
 	}
 
 	/**
@@ -62,9 +105,16 @@ class URLSearchParams implements Stringable {
 	 * of all search parameters.
 	 *
 	 * @param string $name The name of the parameter to be deleted.
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/delete
 	 */
 	public function delete(string $name):void {
+		$name = rtrim($name, "[]");
+		if(!isset($this->kvp[$name])) {
+			return;
+		}
 
+		unset($this->kvp[$name]);
 	}
 
 	/**
@@ -74,19 +124,45 @@ class URLSearchParams implements Stringable {
 	 * order as they appear in the query string. The key and value of each
 	 * pair are string objects.
 	 *
-	 * @return array<string>
+	 * @return Generator<string, string>
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/entries
 	 */
-	public function entries():array {
-
+	public function entries():Generator {
+		foreach($this->kvp as $key => $value) {
+			if(is_array($value)) {
+				foreach($value as $subValue) {
+					yield "{$key}[]" => $subValue;
+				}
+			}
+			else {
+				yield $key => $value;
+			}
+		}
 	}
 
 	/**
 	 * The forEach() method of the URLSearchParams interface allows
 	 * iteration through all values contained in this object via a
 	 * callback function.
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/forEach
 	 */
 	public function forEach(callable $callback):void {
-
+		foreach($this->kvp as $key => $value) {
+			if(is_array($value)) {
+				foreach($value as $subValue) {
+					call_user_func(
+						$callback,
+						"{$key}[]",
+						$subValue
+					);
+				}
+			}
+			else {
+				call_user_func($callback, $key, $value);
+			}
+		}
 	}
 
 	/**
@@ -96,9 +172,21 @@ class URLSearchParams implements Stringable {
 	 * Note: This class implements type-safe getters, getInt, getBool, etc.
 	 *
 	 * @param string $name The name of the parameter to return.
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/get
 	 */
 	public function get(string $name):mixed {
+		$name = rtrim($name, "[]");
+		$value = $this->kvp[$name] ?? null;
+		if(!$value) {
+			return null;
+		}
 
+		if(is_array($value)) {
+			return $value[0];
+		}
+
+		return $value;
 	}
 
 	/**
@@ -107,9 +195,17 @@ class URLSearchParams implements Stringable {
 	 *
 	 * @param string $name The name of the parameter to return.
 	 * @return array<string>
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/getAll
 	 */
 	public function getAll(string $name):array {
+		$name = rtrim($name, "[]");
+		$value = $this->kvp[$name] ?? [];
+		if(!is_array($value)) {
+			$value = [$value];
+		}
 
+		return $value;
 	}
 
 	/**
@@ -119,9 +215,12 @@ class URLSearchParams implements Stringable {
 	 *
 	 * @param string $name The name of the parameter to find.
 	 * @return bool
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/has
 	 */
 	public function has(string $name):bool {
-
+		$name = rtrim($name, "[]");
+		return isset($this->kvp[$name]);
 	}
 
 	/**
@@ -130,9 +229,21 @@ class URLSearchParams implements Stringable {
 	 * object. The keys are string objects.
 	 *
 	 * @return array<string>
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/keys
 	 */
 	public function keys():array {
+		$keys = [];
+		foreach($this->kvp as $key => $value) {
+			if(is_array($value)) {
+				array_push($keys, "{$key}[]");
+			}
+			else {
+				array_push($keys, $key);
+			}
+		}
 
+		return $keys;
 	}
 
 	/**
@@ -143,9 +254,11 @@ class URLSearchParams implements Stringable {
 	 *
 	 * @param string $name The name of the parameter to set.
 	 * @param string $value The value of the parameter to set.
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/set
 	 */
 	public function set(string $name, string $value):void {
-
+		$this->kvp[$name] = $value;
 	}
 
 	/**
@@ -154,9 +267,11 @@ class URLSearchParams implements Stringable {
 	 * order is according to unicode code points of the keys. This method
 	 * uses a stable sorting algorithm (i.e. the relative order between
 	 * key/value pairs with equal keys will be preserved).
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/sort
 	 */
 	public function sort():void {
-
+		ksort($this->kvp);
 	}
 
 	/**
@@ -165,8 +280,21 @@ class URLSearchParams implements Stringable {
 	 * object. The values are string objects.
 	 *
 	 * @return array<string>
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/values
 	 */
 	public function values():array {
-
+		$values = [];
+		foreach($this->kvp as $value) {
+			if(is_array($value)) {
+				foreach($value as $subValue) {
+					array_push($values, $subValue);
+				}
+			}
+			else {
+				array_push($values, $value);
+			}
+		}
+		return $values;
 	}
 }
---
+++ test/phpunit/URLSearchParamsTest.php
@@ -0,0 +1,297 @@
+<?php
+namespace Gt\Http\Test;
+
+use Gt\Http\Test\Helper\MockedCaller;
+use Gt\Http\URLSearchParams;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class URLSearchParamsTest extends TestCase {
+	public function testToString_empty():void {
+		$sut = new URLSearchParams();
+		self::assertSame("", (string)$sut);
+	}
+
+	public function testToString_onlyQuestionMark():void {
+		$sut = new URLSearchParams("?");
+		self::assertSame("", (string)$sut);
+	}
+
+	public function testToString_oneKeyNoQuestionMark():void {
+		$sut = new URLSearchParams("test");
+		self::assertSame("test=", (string)$sut);
+	}
+
+	public function testToString_oneKeyQuestionMark():void {
+		$sut = new URLSearchParams("?test");
+		self::assertSame("test=", (string)$sut);
+	}
+
+	public function testToString_oneKeyValue():void {
+		$sut = new URLSearchParams("testKey=testValue");
+		self::assertSame("testKey=testValue", (string)$sut);
+	}
+
+	public function testToString_oneKeyValueQuestionMark():void {
+		$sut = new URLSearchParams("?testKey=testValue");
+		self::assertSame("testKey=testValue", (string)$sut);
+	}
+
+	public function testToString_multipleKeyValueFromAssocArray():void {
+		$kvp = [
+			"testKey1" => "value1",
+			"testKey2" => "value2",
+			"testKey3" => "value3",
+		];
+		$sut = new URLSearchParams($kvp);
+		$expected = http_build_query($kvp);
+		self::assertSame($expected, (string)$sut);
+	}
+
+	public function testSize_empty():void {
+		$sut = new URLSearchParams();
+		self::assertSame(0, $sut->size);
+	}
+
+	public function testSize():void {
+		$sut = new URLSearchParams(["one" => 123, "two" => 234]);
+		self::assertSame(2, $sut->size);
+	}
+
+	public function testAppend():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		self::assertSame("name=Cody", (string)$sut);
+	}
+
+	public function testAppend_double():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		self::assertSame("name=Cody&food[]=mushroom&food[]=pepper", (string)$sut);
+	}
+
+	public function testAppend_triple():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		$sut->append("food", "corn");
+		self::assertSame("name=Cody&food[]=mushroom&food[]=pepper&food[]=corn", (string)$sut);
+	}
+
+	public function testDelete_notExists():void {
+		$sut = new URLSearchParams();
+		$sut->delete("nothing");
+		self::assertSame("", (string)$sut);
+	}
+	public function testDelete_single():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		$sut->delete("name");
+		self::assertSame("food[]=mushroom&food[]=pepper", (string)$sut);
+	}
+
+	public function testDelete_multiple():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		$sut->delete("food");
+		self::assertSame("name=Cody", (string)$sut);
+	}
+
+	public function testDelete_multipleWithSquareBrackets():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		$sut->delete("food[]");
+		self::assertSame("name=Cody", (string)$sut);
+	}
+
+	public function testEntries_empty():void {
+		$sut = new URLSearchParams();
+		self::assertSame([], iterator_to_array($sut->entries()));
+	}
+
+	public function testEntries():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+
+		$i = 0;
+		foreach($sut->entries() as $key => $value) {
+			if($i === 0) {
+				self::assertSame("name", $key);
+				self::assertSame("Cody", $value);
+			}
+			if($i === 1) {
+				self::assertSame("food[]", $key);
+				self::assertSame("mushroom", $value);
+			}
+			if($i === 2) {
+				self::assertSame("food[]", $key);
+				self::assertSame("pepper", $value);
+			}
+			$i++;
+		}
+	}
+
+	public function testForEach_empty():void {
+		$sut = new URLSearchParams();
+		$calls = [];
+		$sut->forEach(function()use(&$calls) {
+			array_push($calls, func_get_args());
+		});
+		self::assertEmpty($calls);
+	}
+
+	public function testForEach():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+
+		$calls = [];
+		$sut->forEach(function()use(&$calls) {
+			array_push($calls, func_get_args());
+		});
+		self::assertCount(3, $calls);
+		self::assertSame("name", $calls[0][0]);
+		self::assertSame("Cody", $calls[0][1]);
+
+		self::assertSame("food[]", $calls[1][0]);
+		self::assertSame("mushroom", $calls[1][1]);
+		self::assertSame("food[]", $calls[2][0]);
+		self::assertSame("pepper", $calls[2][1]);
+	}
+
+	public function testGet_empty():void {
+		$sut = new URLSearchParams();
+		self::assertNull($sut->get("something"));
+	}
+
+	public function testGet():void {
+		$sut = new URLSearchParams("name=Cody");
+		self::assertSame("Cody", $sut->get("name"));
+	}
+
+	public function testGet_multiple():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+
+		self::assertSame("mushroom", $sut->get("food"));
+	}
+
+	public function testGet_multipleWithSquareBrackets():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+
+		self::assertSame("mushroom", $sut->get("food[]"));
+	}
+
+	public function testGetAll_empty():void {
+		$sut = new URLSearchParams();
+		self::assertEmpty($sut->getAll("something"));
+	}
+
+	public function testGetAll_single():void {
+		$sut = new URLSearchParams("name=Cody");
+		self::assertSame(["Cody"], $sut->getAll("name"));
+	}
+
+	public function testGetAll():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		self::assertSame(["mushroom", "pepper"], $sut->getAll("food"));
+	}
+
+	public function testGetAll_withSquareBrackets():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		self::assertSame(["mushroom", "pepper"], $sut->getAll("food[]"));
+	}
+
+	public function testHas():void {
+		$sut = new URLSearchParams();
+		self::assertFalse($sut->has("name"));
+		$sut->append("name", "Cody");
+		self::assertTrue($sut->has("name"));
+	}
+
+	public function testHas_withSquareBrackets():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		self::assertTrue($sut->has("food[]"));
+	}
+
+	public function testKeys_empty():void {
+		$sut = new URLSearchParams();
+		self::assertEmpty($sut->keys());
+	}
+
+	public function testKeys():void {
+		$sut = new URLSearchParams("name=Cody&colour=orange");
+		self::assertSame(["name", "colour"], $sut->keys());
+	}
+
+	public function testKeys_multiple():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		self::assertSame(["name", "food[]"], $sut->keys());
+	}
+
+	public function testSet():void {
+		$sut = new URLSearchParams();
+		$sut->set("name", "Cody");
+		self::assertSame("name=Cody", (string)$sut);
+	}
+
+	public function testSet_overwrite():void {
+		$sut = new URLSearchParams();
+		$sut->set("name", "Cody");
+		$sut->set("name", "Scarlett");
+		self::assertSame("name=Scarlett", (string)$sut);
+	}
+
+	public function testSort():void {
+		$sut = new URLSearchParams();
+		$sut->append("name", "Cody");
+		$sut->append("food", "mushroom");
+		$sut->append("food", "pepper");
+		$sut->sort();
+		self::assertSame("food[]=mushroom&food[]=pepper&name=Cody", (string)$sut);
+	}
+
+	public function testValues_empty():void {
+		$sut = new URLSearchParams();
+		self::assertEmpty($sut->values());
+	}
+
+	public function testValues():void {
+		$sut = new URLSearchParams("name=Cody&colour=orange");
+		self::assertSame(["Cody", "orange"], $sut->values());
+	}
+
+	public function testValues_multiple():void {
+		$sut = new URLSearchParams("name=Cody&food[]=mushroom&food[]=pepper");
+		self::assertSame(["Cody", "mushroom", "pepper"], $sut->values());
+	}
+}
